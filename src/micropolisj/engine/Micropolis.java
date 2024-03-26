08aa72eba41f29df02c8de0a07364410a251bdc2
Raeshana <rsookhoo@andrew.cmu.edu>
2024-03-26 10:43:27 -0400

    Revert "Started implementing exceed sprite behaviour"
    This reverts commit e170e40155b156b2f4ee6f42286f2d41fb3f0c77.

--- src/micropolisj/engine/Micropolis.java
+++ src/micropolisj/engine/Micropolis.java
@@ -2318,8 +2318,7 @@ public class Micropolis
 
 	public void makeMonster()
 	{
-		//MonsterSprite monster = (MonsterSprite) getSprite(SpriteKind.GOD);
-		ExceedSprite monster = (ExceedSprite) getSprite(SpriteKind.GOD);
+		MonsterSprite monster = (MonsterSprite) getSprite(SpriteKind.GOD);
 		if (monster != null) {
 			// already have a monster in town
 			monster.soundCount = 1;
@@ -2349,8 +2348,7 @@ public class Micropolis
 	void makeMonsterAt(int xpos, int ypos)
 	{
 		assert !hasSprite(SpriteKind.GOD);
-		//sprites.add(new MonsterSprite(this, xpos, ypos));
-		sprites.add(new ExceedSprite(this, xpos, ypos));
+		sprites.add(new MonsterSprite(this, xpos, ypos));
 	}
 
 	public void makeTornado()
